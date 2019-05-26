b8f10c39042823814b9a48778aef069b7c0fd476
96Agnes <zrh196716@163.com>
2019-05-27 00:18:31 +0800

    init repository

---
+++ src/main/java/com/example/test/springtest/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.test.springtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * created by ZRH on 2019/5/26
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+
+    }
+}
