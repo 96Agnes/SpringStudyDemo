2357de9819308d1f00ee337673a6977a0f494f3e
96Agnes <zrh196716@163.com>
2019-07-30 20:48:53 +0800

    Add question && AvatarUrl

--- src/main/java/com/example/test/springtest/dto/GithubUser.java
+++ src/main/java/com/example/test/springtest/dto/GithubUser.java
@@ -1,43 +1,14 @@
 package com.example.test.springtest.dto;
 
+import lombok.Data;
+
 /*
  *created by ZRH on 2019/6/2
  */
+@Data
 public class GithubUser {
     private String name;
     private Long id;
     private String bio;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    @Override
-    public String toString() {
-        return "GithubUser{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", bio='" + bio + '\'' +
-                '}';
-    }
+    private String avatar_Url;
 }
