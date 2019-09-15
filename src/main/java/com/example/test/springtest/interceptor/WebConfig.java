c24e8797e218e55f5e732f02150b4ec49b45b3df
96Agnes <zrh196716@163.com>
2019-09-15 21:58:28 +0800

    问题详情

--- src/main/java/com/example/test/springtest/interceptor/WebConfig.java
+++ src/main/java/com/example/test/springtest/interceptor/WebConfig.java
@@ -8,9 +8,6 @@ import org.springframework.web.servlet.config.annotation.*;
 //@EnableWebMvc
 //EnableWebMvc代表接手所有的样式配置，所以加上该注解后，前端的样式没有了，无法加载
 public class WebConfig implements WebMvcConfigurer {
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-    }
     @Autowired
     private SessionInterceptor sessionInterceptor;
     @Override
--- src/main/java/com/example/test/springtest/mapper/QuestionMapper.java
+++ src/main/java/com/example/test/springtest/mapper/QuestionMapper.java
@@ -1,5 +1,6 @@
 package com.example.test.springtest.mapper;
 
+import com.example.test.springtest.dto.QuestionDTO;
 import com.example.test.springtest.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -12,13 +13,20 @@ import java.util.List;
 public interface QuestionMapper {
     @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tags) values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     void create(Question question);
+
     @Select("select * from question limit #{size} offset #{offset}")
     List<Question> list(@Param(value = "offset") Integer offset, @Param(value = "size")Integer size);
     //非对象，需要加@param
+
     @Select("select count(1) from question")
     Integer count();
+
     @Select("select * from question where creator = #{userId} limit #{size} offset #{offset}")
     List<Question> listByUserId(@Param(value = "userId") Integer userId, @Param(value = "offset") Integer offset, @Param(value = "size")Integer size);
+
     @Select("select count(1) from question where creator = #{userId}")
     Integer countByUserId(@Param(value = "userId") Integer userId);
+
+    @Select("select * from question where id = #{id}")
+    Question getById(@Param(value = "id")Integer id);
 }
